7f026aa15edd5df1c5e1c447df2ae6ebc4aa61e0
Vishwanath Rao <vishymails@gmail.com>
2023-09-20 00:34:55 +0530

    Add files via upload

---
+++ example5/pages/about.js
@@ -0,0 +1,13 @@
+import Header from '../components/header'
+
+const About = () => (
+  <>
+    <Header />
+    <h1>About page</h1>
+  </>
+)
+
+export default About
+
+
+
---
+++ example6/team-d/decide/static/page.js
@@ -0,0 +1,15 @@
+(function() {
+const element = document.querySelector(".recos");
+const url = element.getAttribute("data-fragment");
+
+console.log(element);
+console.log(url);
+
+window
+      .fetch(url)
+      .then(res => res.text())
+      .then(html => {
+        element.innerHTML = html;
+      });
+
+})();
